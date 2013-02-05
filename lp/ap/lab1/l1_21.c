c9a469569d10299ae81634b9a63b2bb56c248612
Mike Chaliy <mike@chaliy.name>
2013-02-06 01:36:13 +0200

    AP Lab1, added pre-checks, not yet complted

--- lp/ap/lab1/l1_21.c
+++ lp/ap/lab1/l1_21.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-const double e = 2.718281828458;
 const double K = 1.601; 
 
 double a(double x){
-    //printf("asin %g acos %g \n", asin(1/x), acos(pow(e, x)));
-    return asin(1/x) + acos(pow(e, x));
+    //printf("asin %g acos %g \n", asin(1/x), acos(exp(x)));
+    return asin(1/x) + exp(x);
 }
 
 double b(double x){
-    //printf("log10 %g pow %g \n", log10(pow(K,sqrt(x))), pow(e, x));
-    return log10(pow(K,sqrt(x))) * pow(e, x);
+    //printf("log10 %g pow %g \n", log10(pow(K,sqrt(x))), exp(x));
+    return log10(pow(K,sqrt(x))) * exp(x);
 }
 
 double y(double x){
@@ -20,7 +19,6 @@ double y(double x){
     double p2 = pi/2;
 
     if (ab > p2){
-
         return sin(x); 
     } else if (ab == p2){ // TODO: compare delta to epsilone
         return 1.0;
@@ -29,7 +27,23 @@ double y(double x){
     }
 }
 
-void test(double x){
+void test(double x){    
+    if (x == 0){
+        printf("X could not be %g, because of 1/x\n", x);
+        return;
+    }
+    if (1/x > 1 || 1/x < -1){
+        printf("X couls not be %g, because 1/x should be -1...1\n", x);
+        return;
+    }
+    if (exp(x) > 1 || exp(x) < -1){
+        printf("X couls not be %g, because e^x should be -1...1\n", x);
+        return;
+    }
+    if (x < 0){
+        printf("X couls not be %g, because sqrt(x) should be >= 0\n", x);
+        return;
+    }
     printf("x: %g = %g;\n", x, y(x));
 }
 
