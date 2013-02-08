477ef7f89901d8abfd6707de57462ce3280845f5
Mike Chaliy <mike@chaliy.name>
2013-02-08 21:50:19 +0200

    Fixed issue with invalid factorial after 12 in lab2

--- lp/ap/lab2/lab4_21_2.c
+++ lp/ap/lab2/lab4_21_2.c
@@ -3,7 +3,7 @@
 
 #define PI 3.14159265
 
-int factorial(int n){
+double factorial(double n){
   if (n <= 1)
     return 1;
   else    
@@ -14,29 +14,25 @@ double sin2(double x){
     return 0.5 * (1 - cos(2 * x));
 }
 
-double teilor_item(int k, double x){
-    // double u = (pow(2, (2 * k) - 1) * pow(x, 2 * k));
-    // double f = factorial(2 * k);
-    // printf("%i; %g; %g\n", k, u, f);
-    return pow(-1, k + 1) * ((pow(2, (2 * k) - 1) * pow(x, 2 * k)) /  factorial(2 * k) );
+double taylor_item(int k, double x){
+    double u = (pow(2.0, (2 * k) - 1) * pow(x, 2 * k));
+    double f = factorial(2 * k);
+    double s = pow(-1.0, k + 1) * u /  f;
+    //printf("%i; %g; %g; %g\n", k, u, f, s);
+    return s;
 } 
 
-double teilor(double x){
+double taylor(double x){
     const double E = 0.00001;
     int i = 0;
     double result = 0;    
     double ti;
 
-    if (x > 1){
-        printf("Cannot process %g, should be less or equal to 1.0\n", x);
-        return 0.0;
-    }
-
     do{
         i++;
-        ti = teilor_item(i, x);
+        ti = taylor_item(i, x);
         result += ti;
-        printf(">> i: %i; res: %g; ti: %g;\n", i, result, ti);
+        printf(">> i: %i; res: %f; ti: %f;\n", i, result, ti);
     } while(fabs(ti) > E);
 
     return result;
@@ -45,10 +41,9 @@ double teilor(double x){
 void test(double x){
     printf("x = %g\n", x);
     printf("sin2 x = %f\n", sin2(x));
-    printf("teilor x = %f\n", teilor(x));
+    printf("taylor x = %f\n", taylor(x));
 }
 
-int main(void){    
-    // TODO Why we cannot process x < 1.0?
-    test(0.9);
+int main(void){
+    test(4.0);
 }
