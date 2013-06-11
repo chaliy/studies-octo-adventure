8f15e08f4a0443b2dce0ef6f348715bddcad30d7
Mike Chaliy <mike@chaliy.name>
2013-06-11 08:43:58 +0300

    Added initial lexicographical permutation

---
+++ lp/dm/lexicographical_permutation.fsx
@@ -0,0 +1,23 @@
+// lexicographical permutation
+
+let next (ss : int array) =
+
+    let mutable d = false 
+    let mutable i = ss.Length - 1
+    while (d = false) do
+        let l = ss.[i - 1]
+        let r =  ss.[i]
+        printfn "l %A r %A" l r
+        if (l < r) then
+            ss.[i] <- l
+            ss.[i - 1] <- r
+            d <- true
+        i = i - 1
+
+    
+let s = [|4;3;2;5;6;1|]
+printfn "%A" s
+next s
+//printfn "%A" s
+next s
+//printfn "%A" s
