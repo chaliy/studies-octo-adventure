21a56af30871e192549ebf6efc79f62ce07e0049
Mike Chaliy <mike@chaliy.name>
2013-06-14 22:45:20 +0300

    Added lab4

---
+++ lp/ap/lab4/l8_21_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+void outIfLong(FILE *fout, char * w){
+    int len = strlen(w);
+    printf ("%s - %d\n", w, len);
+    if (len > 9){
+        fprintf (fout, "%s\n", w);
+    }
+}
+
+FILE * openFile(const char * name, const char * m){
+    FILE *f = fopen (name, m);
+    if (f == NULL) {
+        fprintf (stderr, "Can't open file %s!\n", name);
+        exit (1);
+    }
+    return f;
+}
+
+int main(void){
+
+    const char separator[] = " .,";
+    const char finName[] = "l8_21_1_in.dat";
+    const char foutName[] = "l8_21_1_out.dat";
+    const int INT_MAX = 32767;
+    char *input = "";
+    char *pch;
+    FILE *fin = openFile (finName, "r");
+    FILE *fout = openFile (foutName, "wt");
+
+    while (fgets(input, INT_MAX, fin)) {  
+        pch = strtok (input, separator);
+        while (pch != NULL)
+        {
+            outIfLong (fout, pch);
+
+            pch = strtok (NULL, separator);
+        }   
+    }
+}
---
+++ lp/ap/lab4/l9_21_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int aCount(char * w){
+    int count = 0;
+    int i;    
+    for(i = 0; i < strlen(w); i++){
+        if (w[i] == 'a' || w[i] == 'A'){
+            count++;
+        }
+    }
+    return count;
+}
+
+FILE * openFile(const char * name, const char * m){
+    FILE *f = fopen (name, m);
+    if (f == NULL) {
+        fprintf (stderr, "Can't open file %s!\n", name);
+        exit (1);
+    }
+    return f;
+}
+
+int main(void){
+    
+    const char finName[] = "l9_21_1_in.dat";
+    const int INT_MAX = 32767;
+    char *input = "";    
+    char *maxAInput = "";
+    int maxA = 0;    
+    FILE *fin = openFile (finName, "r");
+
+    while (fgets(input, INT_MAX, fin)) {        
+        int ac = aCount(input);
+        if (ac >= maxA){
+            maxA = ac;
+            maxAInput = input;
+        }
+    }
+
+    printf ("%d in \"%s\"\n", maxA, maxAInput);
+}
