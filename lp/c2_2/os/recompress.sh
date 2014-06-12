479783bbe8b4e5636d4499321df5094ad6cbf7e2
Mike Chaliy <mike@chaliy.name>
2014-06-12 22:52:02 +0300

    Added SH script to L3 of OS

---
+++ lp/c2_2/os/recompress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo Dowload selected file
+wget -O tmp_file.zip $1
+
+echo Make tempfolder
+mkdir tmp_archive
+
+echo Unzip file to temp folder
+unzip tmp_file.zip -d tmp_archive
+
+echo Remove temp file
+rm tmp_file.zip -f
+
+echo Tar content of the temp folder
+
+tar --directory tmp_archive -cz -f result.tar.gz .
+
+echo Remove temp folder
+# rm tmp_archive -fr
